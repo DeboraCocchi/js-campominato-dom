2faae8481ef8005275847f4dd31638bbef46ef5d
DeboraCocchi <cccdbr@gmail.com>
2022-10-21 17:16:49 +0200

    [proceeding]bombs-created

--- js/script.js
+++ js/script.js
@@ -1,30 +1,35 @@
-/* L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-**Bonus**
-Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;  */
+/*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+**BONUS:**
+1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+****2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste */
 
 const container = document.querySelector('.container');
 const createButton = document.getElementById('create-squares');
-const selector = document.querySelector('#select-rows');
+const levelSelector = document.querySelector('#select-rows');
+const bombsNumber = 16;
+let bombsArray = [];
+let score = 0;
 
 
-createButton.addEventListener('click', initialize);
+createButton.addEventListener('click', play);
 
-function initialize(numElements){
-  console.log(selector.value);
+function play(numElements){
+  console.log(levelSelector.value);
   createButton.classList.add('hide');
-  selector.classList.add('hide');
-  const totalSquares = Math.pow(selector.value,2);
+  levelSelector.classList.add('hide');
+  const totalSquares = Math.pow(levelSelector.value,2);
   console.log(totalSquares);
 
   for(let i=0; i<totalSquares;i++){
     createSquare(i);
   }
+
+  bombsArray= bombsGenerator(totalSquares);
+  console.log(bombsArray);
 }
 
 function createSquare(idNumber){
@@ -39,13 +44,39 @@ function createSquare(idNumber){
 
 function clickSquare(){
   console.log(this.identityNumber);
-  this.innerHTML = this.identityNumber;
-  this.classList.add('lightblue');
+  
+  if(!bombsArray.includes(this.identityNumber)){
+    this.innerHTML = this.identityNumber;
+    this.classList.add('lightblue');
+    score++;
+    console.log(score);
+    const totalSquares = Math.pow(levelSelector.value,2);
+    if(score===totalSquares-bombsNumber){
+      console.log('hai vinto!');
+    }else{
+      console.log('hai perso!');
+    }
+  }
+  
 }
 
 
 function calcSpaceForCss(){
-  return `calc(100% / ${selector.value})`;
+  return `calc(100% / ${levelSelector.value})`;
 }
 
 
+function bombsGenerator(totalSquares){
+  const bombsGenerated = [];
+  while(bombsGenerated.length<bombsNumber){
+   const singleBomb = generateBombRdmNumber(1,totalSquares);
+   if(!bombsGenerated.includes(singleBomb)){
+     bombsGenerated.push(singleBomb);
+   }
+  }
+  return bombsGenerated;
+}
+
+function generateBombRdmNumber(min,max){
+  return Math.floor(Math.random()*(max - min)+min);
+}
