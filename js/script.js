26f866388c3775bb1ecba2caad581d3d5b1b0e9a
DeboraCocchi <cccdbr@gmail.com>
2022-10-24 00:14:32 +0200

    [end-result]with-reset

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
-/*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-I numeri nella lista delle bombe non possono essere duplicati.
-In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
-La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
-**BONUS:**
-1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
-****2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste */
+
 
 const container = document.querySelector('.container');
 const createButton = document.getElementById('create-squares');
 const levelSelector = document.querySelector('#select-rows');
 const bombsNumber = 16;
+const newGame = document.createElement('button');
+newGame.className = 'btn-newgame';
+newGame.innerHTML='Nuova Partita';
+const output = document.getElementById('result');
 let bombsArray = [];
 let score = 0;
 
@@ -18,9 +15,11 @@ let score = 0;
 createButton.addEventListener('click', play);
 
 function play(numElements){
-  console.log(levelSelector.value);
+  
   createButton.classList.add('hide');
   levelSelector.classList.add('hide');
+  container.classList.remove('hide');
+
   const totalSquares = Math.pow(levelSelector.value,2);
   console.log(totalSquares);
 
@@ -38,27 +37,51 @@ function createSquare(idNumber){
   square.identityNumber = idNumber + 1;
   square.style.width = calcSpaceForCss();
   square.style.height = calcSpaceForCss();
-  square.addEventListener('click', clickSquare);
+  square.addEventListener('click', clickSquare, {once : true});
   container.append(square); 
 }
 
 function clickSquare(){
-  console.log(this.identityNumber);
-  
+
+  output.classList.remove('hide'); 
+
   if(!bombsArray.includes(this.identityNumber)){
-    this.innerHTML = this.identityNumber;
-    this.classList.add('lightblue');
-    score++;
-    console.log(score);
-    const totalSquares = Math.pow(levelSelector.value,2);
-    if(score===totalSquares-bombsNumber){
-      console.log('hai vinto!');
-    }else{
-      console.log('hai perso!');
-    }
+      this.innerHTML = this.identityNumber;
+        if(!(this.identityNumber % 3)){
+          this.classList.add('purple');
+        }else if(!(this.identityNumber % 4)){
+          this.classList.add('orange');
+        }else{
+          this.classList.add('blue');
+        }
+      
+
+      score++;
+
+      const totalSquares = Math.pow(levelSelector.value,2);
+   
+      
+      if(score===totalSquares-bombsNumber){
+        output.innerHTML = `Hai vinto il premio Artificiere! Hai raggiunto il punteggio massimo di ${score} punti!`
+        container.append(newGame); 
+      }
+    
+  }else{
+      showBombs();
+      if(score===1){
+      output.innerHTML = `Hai cliccato su una bomba e hai perso! Il tuo punteggio è di ${score} punto.`
+      }else{
+      output.innerHTML = `Hai cliccato su una bomba e hai perso! Il tuo punteggio è di ${score} punti.`
+      }
+  
+      container.append(newGame);  
+    
   }
   
+  newGame.addEventListener('click', reset);
+  
 }
+
 
 
 function calcSpaceForCss(){
@@ -77,6 +100,39 @@ function bombsGenerator(totalSquares){
   return bombsGenerated;
 }
 
+
+
+
 function generateBombRdmNumber(min,max){
-  return Math.floor(Math.random()*(max - min)+min);
+  return Math.floor(Math.random()*(max - min+1)+min);
 }
+
+
+
+
+function showBombs(){
+  const squares = document.getElementsByClassName('square');
+  for (i=0;i<squares.length;i++){
+    if(bombsArray.includes(squares[i].identityNumber)){
+      squares[i].classList.add('bomb');
+      console.log('hai beccato la bomba! punteggio='+score);
+      const cover = document.createElement('div');
+      cover.className = 'disable-grid';
+      container.append(cover);
+  
+    }
+  }
+}
+
+function reset(){
+  score = 0;
+  bombsArray=[];
+  output.innerHTML='';
+  container.innerHTML='';
+  output.classList.add('hide');
+  container.classList.add('hide');
+  createButton.classList.remove('hide');
+  levelSelector.classList.remove('hide');
+
+};
+
